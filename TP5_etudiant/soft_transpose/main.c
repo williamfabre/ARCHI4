601a51ebafe46925d77320f82ce8a0c83441017f
William Fabre <contact@williamfabre.fr>
2019-10-25 00:03:52 +0200

    transpose first try

--- TP5_etudiant/soft_transpose/main.c
+++ TP5_etudiant/soft_transpose/main.c
@@ -16,106 +16,115 @@ unsigned char 	buf_in[NLINES][NPIXELS];
 ////////////////////////////////////////
 __attribute__((constructor)) void load()
 {
-    int image = 0;
+	int image = 0;
 
-    tty_printf(" !!!  Processor %d running !!!\n", procid());
+	tty_printf(" !!!  Processor %d running !!!\n", procid());
 
-    while(image < NIMAGES) 
-    {
-        while ( buf_in_empty == FALSE ) 	// synchro
-        {
-            asm volatile ( " nop" ); 
-        }
-        
-        tty_printf("\n *** Starting load for image %d *** at cycle %d \n", image, proctime());
-        if( ioc_read(image*NBLOCKS, buf_in, NBLOCKS) )
-        {
-            tty_printf("echec ioc_read\n");
-            exit();
-        }
+	while(image < NIMAGES) 
+	{
+		while ( buf_in_empty == FALSE ) 	// synchro
+		{
+			asm volatile ( " nop" ); 
+		}
 
-        if ( ioc_completed() )
-        {
-            tty_printf("echec ioc_completed\n");
-            exit();
-        }
-        buf_in_empty = FALSE;
-        tty_printf(" *** Completing load for image %d *** at cycle %d \n", image, proctime());
-        image++;
-    } // end while image      
-    exit();
+		tty_printf("\n *** Starting load for image %d *** at cycle %d \n", image, proctime());
+		if( ioc_read(image*NBLOCKS, buf_in, NBLOCKS) )
+		{
+			tty_printf("echec ioc_read\n");
+			exit();
+		}
+
+		if ( ioc_completed() )
+		{
+			tty_printf("echec ioc_completed\n");
+			exit();
+		}
+		buf_in_empty = FALSE;
+		tty_printf(" *** Completing load for image %d *** at cycle %d \n", image, proctime());
+		image++;
+	} // end while image      
+	exit();
 } // end load()
 
 /////////////////////////////////////////////
 __attribute__((constructor)) void transpose()
 {
-    int l,p;
-    int image = 0;
+	int l,p;
+	int image = 0;
 
-    tty_printf(" !!!  Processor %d running !!!\n", procid());
+	tty_printf(" !!!  Processor %d running !!!\n", procid());
 
-    while( image < NIMAGES )
-    { 
-        while( (buf_in_empty == TRUE) || (buf_out_empty == FALSE) )	// synchro
-        {
-            asm volatile ( " nop" ); 
-        }
+	while( image < NIMAGES )
+	{ 
+		while( (buf_in_empty == TRUE) || (buf_out_empty == FALSE) )	// synchro
+		{
+			asm volatile ( " nop" ); 
+		}
 
-        tty_printf("\n *** Starting transpose for image %d *** at cycle %d \n", image, proctime());
-        for( l=0 ; l<NLINES ; l++)
-        {
-            for( p=0 ; p<NPIXELS ; p++)
-            {
-                    buf_out[l][p] = buf_in[p][l];
-            } 
-        }
+		tty_printf("\n *** Starting transpose for image %d *** at cycle %d \n", image, proctime());
+		for( l=0 ; l<NLINES ; l++)
+		{
+			for( p=0 ; p<NPIXELS ; p++)
+			{
+				buf_out[l][p] = buf_in[p][l];
+			} 
+		}
 
-        buf_in_empty = TRUE;
-        buf_out_empty = FALSE;
-        tty_printf(" *** Completing transpose for image %d *** at cycle %d \n", image, proctime());
-        image++;
-    } // end while image
-    exit();
+		buf_in_empty = TRUE;
+		buf_out_empty = FALSE;
+		tty_printf(" *** Completing transpose for image %d *** at cycle %d \n", image, proctime());
+		image++;
+	} // end while image
+	exit();
 } // end transpose
 
 ///////////////////////////////////////////
 __attribute__((constructor)) void display()
 {
-    int	image = 0;
+	int	image = 0;
 
-    tty_printf(" !!!  Processor %d running !!!\n", procid());
+	tty_printf(" !!!  Processor %d running !!!\n", procid());
 
-    while(image < NIMAGES)
-    {
-        while( buf_out_empty == TRUE )  // synchro
-        {
-            asm volatile ( " nop" ); 
-        }
+	while(image < NIMAGES)
+	{
+		while( buf_out_empty == TRUE )  // synchro
+		{
+			asm volatile ( " nop" ); 
+		}
 
-        tty_printf("\n *** starting display for image %d at cycle %d\n", image, proctime());
-        if ( fb_write(0, buf_out, NLINES*NPIXELS) )
-        {
-                tty_printf("echec fb_write\n");
-                exit();
-        }
+		tty_printf("\n *** starting display for image %d at cycle %d\n", image, proctime());
+		if ( fb_write(0, buf_out, NLINES*NPIXELS) )
+		{
+			tty_printf("echec fb_write\n");
+			exit();
+		}
 
-        if ( fb_completed() )
-        {
-            tty_printf("echec ioc_completed\n");
-            exit();
-        }
+		if ( fb_completed() )
+		{
+			tty_printf("echec ioc_completed\n");
+			exit();
+		}
 
-        buf_out_empty = TRUE;
-        tty_printf(" *** completing display for image %d at cycle %d\n", image, proctime());
-        image++;
-    } // end while image
-    exit();
+		buf_out_empty = TRUE;
+		tty_printf(" *** completing display for image %d at cycle %d\n", image, proctime());
+		image++;
+	} // end while image
+	exit();
 } // end display
 
 ////////////////////////////////////////
 __attribute__((constructor)) void idle()
 {
-    tty_printf(" !!! No task allocated to Processor %d !!!\n", procid() );
-    exit();
+	volatile char c;
+
+	while (1)
+	{
+		tty_puts("wait...\n");
+		tty_getc_irq( (void*)&c);
+		if (c == 'q')
+			break;
+	}
+	tty_printf(" !!! No task allocated to Processor %d !!!\n", procid() );
+	exit();
 } // end idle 
 
